930898e8584df5dc902bdefac5223d3a706cbcc3
David C <d.chia@proton.me>
2022-08-02 01:50:12 +0200

    Add number input from button to array

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 //declare variables globally: stores inputs and display value
 
-let firstNum = 0;
+let firstNum;
 let secondNum;
-let cache = 0;
+let cache;
+let inputArray = [];
 let displayValue = 0;
 
 
 //declare variables globally: buttons as js elements
+
+const numButtons = document.querySelectorAll(".numButtons")
 
 const one = document.querySelector("#one");
 const two = document.querySelector("#two");
@@ -57,16 +60,63 @@ function operate(firstNum, calculation, secondNum) {
 
 
 //display shows displayValue
+//function to update display value for easy reuse
+//by default or initial load runs function and shows 0 from initial display value
 
 const displayContent = document.querySelector("#displayContent");
 
-displayContent.innerText = displayValue;
+function displayUpdate() {
+    displayContent.innerText = displayValue};
+
+displayUpdate();
+
+
+//add event listeners for number buttons
+
+numButtons.forEach(number => number.addEventListener("click", numInput));
+
+
+//declare function to push value of button into inputArray;
+//event e when used with event listener is click of button
+//update display value to show correct number
+
+function numInput(e) {
+    inputArray.push(+e.srcElement.innerText);
+    displayValue = +inputArray.join("");
+    displayUpdate();
+};
+
+
+
+//add event listener to AC 
+//add clear all by emptying and zeroing arrays and inputs
+
+AC.addEventListener("click", () => {
+    cache = 0;
+    inputArray = [];
+    displayValue = 0;
+    displayUpdate();
+    }
+)
 
 
 
 
 
-//pressing equal updates displayValue
+
+
+//notes, ideas:
+//user input numbers are stored in cache array
+//array transformed into int and stored in firstNum at every input
+//when button for calculation is pressed, mode is toggled or stored
+
+//declare variable for modes that are called in operator
+// let mode = inputMode;
+
+//also initiates input into second array (not in default input mode)
+//array transformed into int and stored in secondNum at every input
+//pressing equal runs operate function with all three parameters
+//pressing equal updates displayValue = result
 
 
 
@@ -74,3 +124,6 @@ displayContent.innerText = displayValue;
 //if button that is not calculation (=number button) is pressed
 //while result is displayed (somehow connected to cache?)
 //reset and start with new input
+
+
+//when press oepration --> store whatever is in array in cache --> empty input array  --> fill input array again (default 0?) --> operation or equals puts number in cache again --> display cache as result
